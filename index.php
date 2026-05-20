d2c5adb9f2e25f63fe918b08153cf01ea9bb2312
FarelFauza <fauzafarel59@gmail.com>
2026-05-20 11:21:01 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,57 +1,57 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Membuat CRUD Dengan PHP Dan MySQL - Menampilkan data dari database</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="judul">
+        <h1>Membuat CRUD Dengan PHP Dan MySQL</h1>
+        <h2>Menampilkan data dari database</h2>
+    </div>
+    <br/>
 
-require_once "tagihanListrik.php";
-require_once "tagihanRepositori.php";
-
-/**
- * Fungsi untuk memformat angka ke format Rupiah.
- * Dideklarasikan hanya satu kali untuk menghindari Fatal Error.
- */
-if (!function_exists('formatRupiah')) {
-    function formatRupiah($angka) {
-        return "Rp " . number_format($angka, 0, ',', '.');
+    <?php
+    if(isset($_GET['pesan'])){
+        $pesan = $_GET['pesan'];
+        if($pesan == "input"){
+            echo "Data berhasil di input.";
+        }else if($pesan == "update"){
+            echo "Data berhasil di update.";
+        }else if($pesan == "hapus"){
+            echo "Data berhasil di hapus.";
+        }
     }
-}
+    ?>
+    <br/>
+    <a class="tombol" href="input.php">+ Tambah Data Baru</a>
 
-// Inisialisasi Repository
-$repo = new tagihanRepositori();
-$data = $repo->getAll();
-
-echo "<h2>DATA TAGIHAN LISTRIK</h2>";
-
-// Mulai Tabel
-echo "<table border='1' cellpadding='6' cellspacing='0' style='border-collapse: collapse; width: 100%; max-width: 600px;'>";
-echo "<thead>
-        <tr style='background-color: #f2f2f2;'>
+    <h3>Data user</h3>
+    <table border="1" class="table">
+        <tr>
             <th>No</th>
             <th>Nama</th>
-            <th>KWH</th>
-            <th>Total Tagihan</th>
-        </tr> </thead>";
-
-echo "<tbody>";
-
-$no = 1;
-
-// Loop data dari repository
-if (is_array($data)) {
-    foreach ($data as $d) {
-        // Buat objek baru untuk setiap baris data
-        $obj = new tagihanListrik();
-        
-        // Mengisi data ke objek
-        $obj->setData($d["nama"], $d["kwh"]);
-
-        echo "<tr>";
-        echo "<td align='center'>" . $no++ . "</td>";
-        echo "<td>" . $obj->getNama() . "</td>";
-        echo "<td align='center'>" . $obj->getKwh() . "</td>";
-        echo "<td align='right'>" . formatRupiah($obj->hitungTotal()) . "</td>";
-        echo "</tr>";
-    }
-}
-
-echo "</tbody>";
-echo "</table>";
-
-?>
+            <th>Alamat</th>
+            <th>Pekerjaan</th>
+            <th>Opsi</th>
+        </tr>
+        <?php
+        include 'koneksi.php';
+        $query_mysql = mysqli_query($koneksi, "SELECT * FROM user")or die(mysqli_error());
+        $nomor = 1;
+        while($data = mysqli_fetch_array($query_mysql)){
+        ?>
+        <tr>
+            <td><?php echo $nomor++; ?></td>
+            <td><?php echo $data['nama']; ?></td>
+            <td><?php echo $data['alamat']; ?></td>
+            <td><?php echo $data['pekerjaan']; ?></td>
+            <td>
+                <a class="edit" href="edit.php?id=<?php echo $data['id']; ?>">Edit</a> |
+                <a class="hapus" href="hapus.php?id=<?php echo $data['id']; ?>">Hapus</a>
+            </td>
+        </tr>
+        <?php } ?>
+    </table>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "si_gudang";
+
+
+$koneksi = mysqli_connect('localhost', 'root', '', 'si_gudang');
+
+
+?>
